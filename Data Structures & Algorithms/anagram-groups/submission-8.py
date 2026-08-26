2bda160f4281f3c3cddc3ccbfdc8874b698d9cf1
Karl <karlaazh2007@gmail.com>
2026-08-26 18:25:58 +0300

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = {}
+
+        for string in strs:
+            key = ''.join(sorted(string))
+
+            if key in hashmap:
+                hashmap[key].append(string)
+            else:
+                hashmap[key] = [string]
+        
+        return list(hashmap.values())
